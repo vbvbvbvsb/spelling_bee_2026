79554aef3f1c4f7411cbd6b76b7d7f0decfcc4bb
vbvbvbvsb <vbroccoli@gglwins.com>
2026-06-06 14:13:10 -0400

    Initial Commit

---
+++ SpellingBee26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpellingBee26
+//
+//  Created by Victor Broccoli on 6/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpellingBee26/SpellingBee26App.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBee26App.swift
+//  SpellingBee26
+//
+//  Created by Victor Broccoli on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBee26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
